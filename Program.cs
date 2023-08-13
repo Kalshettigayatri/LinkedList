9799e14f55b095feba8c52ff40055dcca3376cb5
kalshettigayatri <gayatrikalshetti3@gmail.com>
2023-08-13 17:48:05 +0530

    LinkedList program

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LInkedList
+{
+    internal class LinkedList
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to LinkedList program!");
+        }
+    }
+}
